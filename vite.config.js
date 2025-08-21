dae237facfcf74ed45092ea14674b1685ad15049
yasir azman <yasirazman96@gmail.com>
2025-08-21 12:56:52 +0800

    Fix vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/kelas-ngaji-albiruni/', // Replace with your GitHub repo name
+  base: '/kelas-ngaji-albiruni/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
